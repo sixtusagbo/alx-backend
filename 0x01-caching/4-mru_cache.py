591ec9a8414c2e928df5ffe49adfbd5abb7dd165
Sixtus Miracle Agbo <Sixtus Agbo>
2023-10-26 05:28:11 +0000

    Keeping track of timestamp in Most Recently Used Cache

--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """MRU caching"""
+from datetime import datetime
+
 BaseCaching = __import__("base_caching").BaseCaching
 
 
@@ -16,17 +18,15 @@ class MRUCache(BaseCaching):
             return
         self.cache_data[key] = item
         if len(self.cache_data.keys()) > BaseCaching.MAX_ITEMS:
-            most_used_key = max(self.usage, key=lambda k: self.usage[k])
-            del self.cache_data[most_used_key]
-            del self.usage[most_used_key]
-            print("DISCARD: {}".format(most_used_key))
-        self.usage[key] = (
-            self.usage[key] + 1 if self.usage.get(key) is not None else 1
-        )
+            most_recently_used = max(self.usage, key=lambda k: self.usage[k])
+            del self.cache_data[most_recently_used]
+            del self.usage[most_recently_used]
+            print("DISCARD: {}".format(most_recently_used))
+        self.usage[key] = datetime.now()
 
     def get(self, key):
         """Return an item from the cache"""
         if key is None or self.cache_data.get(key) is None:
             return None
-        self.usage[key] += 1
+        self.usage[key] = datetime.now()
         return self.cache_data[key]
