1df8dbc09bdd88be21d628afebfde92fc7688968
Sixtus Agbo <miracleagbosixtus@gmail.com>
2023-11-02 11:21:07 +0000

    Mock logging in

---
+++ 0x02-i18n/5-app.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python3
+"""Basic Flask app"""
+from flask import Flask, g, render_template, request
+from flask_babel import Babel
+
+
+class Config(object):
+    """Flask configs"""
+
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
+
+@babel.localeselector
+def get_locale():
+    """Get request locale"""
+    locale = request.args.get("locale")
+    if locale and locale in app.config["LANGUAGES"]:
+        return locale
+    return request.accept_languages.best_match(app.config["LANGUAGES"])
+
+
+def get_user(login_as=None):
+    """Return a user dictionary or None"""
+    if login_as is not None:
+        try:
+            return users.get(int(login_as))
+        except ValueError:
+            return None
+    else:
+        return login_as
+
+
+@app.before_request
+def before_request():
+    """Use `get_user` to find a user if any
+    and set it as a global on `flask.g.user`
+    """
+    user = get_user(login_as=request.args.get("login_as"))
+    if user is not None:
+        g.user = user
+    else:
+        g.user = None
+
+
+@app.route("/")
+def home():
+    """Home page route"""
+    return render_template("5-index.html", user=g.user)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
