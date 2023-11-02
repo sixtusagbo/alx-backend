d792c033c1f2d1fffa2644ad298d3a06ee3a8733
Sixtus Agbo <miracleagbosixtus@gmail.com>
2023-11-02 04:06:07 +0000

    Remove config file

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -1,11 +1,20 @@
 #!/usr/bin/env python3
 """Basic Flask app"""
-from flask import Config, Flask, render_template
+from flask import Flask, render_template
 from flask_babel import Babel
 
+
+class Config(object):
+    """Flask configs"""
+
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
 app = Flask(__name__)
-babel = Babel(app)
 app.config.from_object(Config)
+babel = Babel(app)
 
 
 @app.route("/")
