8c04f127818673b9b4d7914be5d1b0c9c9831c33
Sixtus Agbo <miracleagbosixtus@gmail.com>
2024-09-25 15:56:54 +0000

    Remove async from 1-redis-op

--- 0x03-queuing_system_in_js/1-redis_op.js
+++ 0x03-queuing_system_in_js/1-redis_op.js
@@ -2,13 +2,11 @@ import { createClient, print } from "redis";
 
 let client;
 
-(async () => {
-  client = createClient();
+client = createClient();
 
-  client.on("error", (err) => console.log(`Redis client not connected to the server: ${err}`));
+client.on("error", (err) => console.log(`Redis client not connected to the server: ${err}`));
 
-  client.on("ready", () => console.log("Redis client connected to the server"));
-})();
+client.on("ready", () => console.log("Redis client connected to the server"));
 
 const setNewSchool = (schoolName, value) => client.set(schoolName, value, print);
 
