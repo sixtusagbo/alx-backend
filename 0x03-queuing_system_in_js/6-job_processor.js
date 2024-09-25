61188f47ab3f8a1a089985b7eb9ea4d5755346b6
Sixtus Agbo <miracleagbosixtus@gmail.com>
2024-09-25 18:27:57 +0000

    Create the Job processor

---
+++ 0x03-queuing_system_in_js/6-job_processor.js
@@ -0,0 +1,13 @@
+const { createQueue } = require('kue');
+
+const queue = createQueue();
+
+const sendNotification = (phoneNumber, message) => {
+  console.log(`Sending notification to ${phoneNumber}, with message: ${message}`);
+};
+
+queue.process('push_notification_code', (job, done) => {
+  const { phoneNumber, message } = job.data;
+  sendNotification(phoneNumber, message);
+  done();
+});
